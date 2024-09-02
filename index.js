05d0615c67287b3d6949f1e16380ed7812d94c71
Gasto Enock <141215903+gastoenock@users.noreply.github.com>
2024-09-02 16:52:33 +0300

    Update index.js, adding loading with define function
    makes easy for a module to load

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-import Carbon from "./src/Carbon.js";
+// import Carbon from "./src/Carbon.js";
 
-    let date1 = Carbon.parse('2025-08-14 02:53:01.0 +00:00 (East Africa Time)')
-    // let date1 = Carbon.createFromDate(2025,8,14)
-    // let date1 = Carbon.createFromFormat("dd-mm-yyyy", '2025-08-14')
-    // .getTime()
-    // .diffForHumans()
-    // .diffInYears('Thu Dec 04 2020 16:17:46 GMT+0300 (East Africa Time)')
-    // .format('L -mm-yyyy')
-    // .format('dd-mm-yyyy l,L'); 
-    // .format('d-m-Y F, A jS')
-    // .year(2021)
-    // .month(2)
-    // .day(15)
-    // .hour(2)
-    // .minute(15)
-    .addMonths(1)
-    .format('d/m/Y h:i:s A')
-    // .toDateTimeString()
-    console.log(Carbon);
-    console.log(date1);
+//     let date1 = Carbon.parse('2025-08-14 02:53:01.0 +00:00 (East Africa Time)')
+//     // let date1 = Carbon.createFromDate(2025,8,14)
+//     // let date1 = Carbon.createFromFormat("dd-mm-yyyy", '2025-08-14')
+//     // .getTime()
+//     // .diffForHumans()
+//     // .diffInYears('Thu Dec 04 2020 16:17:46 GMT+0300 (East Africa Time)')
+//     // .format('L -mm-yyyy')
+//     // .format('dd-mm-yyyy l,L'); 
+//     // .format('d-m-Y F, A jS')
+//     // .year(2021)
+//     // .month(2)
+//     // .day(15)
+//     // .hour(2)
+//     // .minute(15)
+//     .addMonths(1)
+//     .format('d/m/Y h:i:s A')
+//     // .toDateTimeString()
+//     console.log(Carbon);
+//     console.log(date1);
+
+define( [
+    "./src/Carbon.js"
+
+], function( Carbon ) {
+
+"use strict";
+
+return Carbon;
+
+});
+
+module.exports.Carbon = Carbon
